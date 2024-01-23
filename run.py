ba674a4a87cbcad3b3cf85fa53ba2135db57220a
LinusMolin98 <>
2024-01-23 13:24:36 +0000

    Creating a function that makes the game actually run, wich also identifies what happens in the game, if a ship is hit or not etc

--- run.py
+++ run.py
@@ -31,6 +31,7 @@ def place_ship(board, ship_size):
             for row, col in ship_coordinates:
                 board[row][col] = 'S'
             break
+
 def player_turn(board):
     while True:
         try:
@@ -42,3 +43,34 @@ def player_turn(board):
                 print("Invalid input. Please enter valid row and column numbers.")
         except ValueError:
             print("Invalid input. Please enter valid integers.")        
+
+def play_battleship(size, num_ships):
+    player_board = generate_board(size)
+
+    for _ in range(num_ships):
+        place_ship(player_board, 3)  # You can change the ship size as per your preference
+
+    while True:
+        print("Player Board:")
+        print_board(player_board)
+
+        player_guess = player_turn(player_board)
+        result = player_board[player_guess[0]][player_guess[1]]
+
+        if result == 'S':
+            print("Congratulations! You hit a ship!")
+            player_board[player_guess[0]][player_guess[1]] = 'X'
+        elif result == 'X':
+            print("You've already guessed that one. Try again.")
+        else:
+            print("Sorry, you missed.")
+
+        # Check if all ships are sunk
+        if all('S' not in row for row in player_board):
+            print("Congratulations! You sank all the ships. You win!")
+            break
+
+if __name__ == "__main__":
+    board_size = 8  
+    num_ships = 3   # You can change the number of ships as per your preference
+    play_battleship(board_size, num_ships)            
