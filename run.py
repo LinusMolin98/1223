0faef76b32bb5d0b7caa1df61fd5700381840e49
LinusMolin98 <>
2024-01-23 13:15:45 +0000

    def board so that it will be generated in correct size

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+def generate_board(size):
+    return [['O' for _ in range(size)] for _ in range(size)]
+
